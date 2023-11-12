e2028bd44dcf66a860021328aad421b316337338
qngg <124476435+qngg@users.noreply.github.com>
2023-11-12 03:56:52 +0000

    added costumes and wordlist

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <math.h>
+#include <time.h>
+#include <string>
+#include <vector>
+#include <fstream>
+
+std::vector<std::string> importTxtFileAsVec(std::string txtFileLoc)
+{
+    std::string line;
+    std::ifstream txtFile(txtFileLoc);
+    std::vector<std::string> vec;
+    if (txtFile.is_open())
+    {
+        while(getline(txtFile, line))
+        {
+            vec.push_back(line);
+        }
+        txtFile.close();
+    }
+    else throw("File is not found");
+    return vec;
+}
+
+int randint(int min, int max)
+{
+    return rand() % (max - min) + min;
+}
 
 int main()
 {
-    std::cout << "Hello world!";
+    srand(time(nullptr));
+    std::vector<std::string> wordList = importTxtFileAsVec("wordlist.txt");
+    int r = 0;
+    while (r++ < 10) std::cout << wordList.at(randint(0, wordList.size() - 1)) << "\n";
     return 0;
 }
